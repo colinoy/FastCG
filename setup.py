38a30e8bd66bc03aa747704545c9e7f43a7d4999
Linoy <colinoy@gmail.com>
2023-07-18 13:34:41 +0300

    Update version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 DESCRIPTION = 'Counterfactual Generation Package'
 LONG_DESCRIPTION = 'A package that makes it easy generate counterfactuals in a fast manner'
 
